2d01981a89fc53dcd7c5a153e23f0d42f42f902c
Gokul <gokulmehta1777@gmail.com>
2023-09-09 19:58:59 +0530

    Creating Database Table & SignUp Form

--- signup.php
+++ signup.php
@@ -1,3 +1,27 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $showAlert = false;
+  $showError = false;
+  include 'partials/_dbconnect.php';
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $cpassword = $_POST["cpassword"];
+  $exist = false;
+  if (($password == $cpassword) && $exist==false){
+     $sql = "INSERT INTO `users` (`username`, `password`, `dt`) VALUES ('$username', '$password', current_timestamp())";
+     $result = mysqli_query($conn, $sql);
+     if($result){
+      $showAlert = true;
+     }
+  }
+  else{
+    $showError = "Passwords do not match";
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,12 +44,31 @@
   </head>
   <body>
     <?php require 'partials/_nav.php'   ?>
-    <div class="container">
+    <?php
+    if($showAlert){
+      echo'<div class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Success!</strong> Your account is now created and you can login.
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
+      </div>';
+    }
+    if($showError){
+      echo'<div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>Error!</strong> '. $showError.'
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
+      </div>';
+    }
+      ?>
+
+    <div class="container my-4">
         <h1 class = "text-center">SignUp to our Website</h1>
         <form action="/loginsystem/signup.php" method="post">
   <div class="form-group ">
     <label for="username">Username</label>
-    <input type="email" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+    <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
     
   </div>
   <div class="form-group ">
