b3bf47d47265e1a742771237e9e04031d95419b3
Gokul <gokulmehta1777@gmail.com>
2023-09-21 12:36:23 +0530

    Adding Logout Functionality

--- partials/_nav.php
+++ partials/_nav.php
@@ -1,4 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<?php
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  $loggedin = true;
+}
+else{ 
+  $loggedin = false;
+}
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="/loginsystem">iSecure</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,21 +15,27 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/loginsystem/welcome.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
+      </li>';
+
+      if(!$loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/loginsystem/login.php">Login</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/loginsystem/signup.php">SingUp</a>
-      </li>
-      <li class="nav-item">
+      </li>';
+      }
+if($loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/loginsystem/logout.php">LogOut</a>
-      </li>
+      </li>';
+}
       
-    </ul>
+    echo '</ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
-</nav>
+</nav>';
+?>
--- welcome.php
+++ welcome.php
@@ -30,13 +30,21 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
   </head>
   <body>
     <?php require 'partials/_nav.php'   ?>
-    Welcome - <?php echo $_SESSION['username'] ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <div class="comtainer my-3">
+    <div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Welcome - <?php echo $_SESSION['username'] ?></h4>
+  <p> Hay how are you doing? Welcome to iSecure. Your are logged in as <?php echo $_SESSION['username'] ?>. Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to logout <a href="/loginsystem/logout.php"> using this link.</p>
+</div>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+    </html>
   </body>
-</html>
-</body>
-</html>
+  </html>
